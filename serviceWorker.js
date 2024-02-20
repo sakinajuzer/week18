6d9e44cce1f9ca9656da8b2f6b0184138a52f151
Sakina Juzer <sakinajuz136@gmail.com>
2024-02-20 10:05:19 +0400

    editted

--- serviceWorker.js
+++ serviceWorker.js
@@ -20,8 +20,16 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', function(e){
     e.respondWith(
         caches.match(e.request).then(function(r){
-            console.log('[Service Worker] fetching resource: '+ e.request.url);
-            return r;
+            return r || fetch(e.request).then(function(response){
+                return caches.open(cacheName).then(function(cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
+        // // caches.match(e.request).then(function(r){
+        // //     console.log('[Service Worker] fetching resource: '+ e.request.url);
+        // //     return r;
+        // })
     );
 });
