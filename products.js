d16c06adbdffe0d881c7c385713d48c7a5a709ad
Sakina Juzer <sakinajuz136@gmail.com>
2024-02-20 09:45:51 +0400

    cache

---
+++ products.js
@@ -0,0 +1,48 @@
+let products =[
+    {
+        id:1001,
+        title:"Cat food 25lb bag",
+        description: "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+        price: 20.00,
+        image:"images/catfood.jpg",
+        availableInventory:10,
+        rating: 2
+    
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+        price: 50.00,
+        image:"images/catfood.jpg",
+        availableInventory:10,
+        rating: 2
+    },
+    // {
+    //     "id":1003,
+    //     "title":"Cat food 25lb bag",
+    //     "description": "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+    //     "price": 90.00,
+    //     "image":"images/catfood.jpg",
+    //     "availableInventory":10,
+    //     "rating": 2
+    // },
+    // {
+    //     "id":1004,
+    //     "title":"Cat food 25lb bag",
+    //     "description": "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+    //     "price": 100.00,
+    //     "image":"images/catfood.jpg",
+    //     "availableinventory":10,
+    //     "rating": 2
+    // },
+    // {
+    //     "id":1005,
+    //     "title":"Cat food 25lb bag",
+    //     "description": "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+    //     "price": 150.00,
+    //     "image":"images/catfood.jpg",
+    //     "availableinventory":10,
+    //     "rating": 2
+    // }
+]
